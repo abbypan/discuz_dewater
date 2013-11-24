47b2c548a7934063c8b6af5a17d262b6c8945f8d
abbypan <abbypan@gmail.com>
2013-11-24 15:58:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+discuz_dewater
+===================
+
+Discuz 论坛 贴子脱水
+
+安装：http://userscripts.org/scripts/show/165947
+
+以 顾漫官网 的 [漫漫作品情况一览](http://www.gumangw.com/forum.php?mod=viewthread&tid=29&extra=page%3D1) 为例
+
+贴子内添加脱水按钮
+![form](dewater_form.png)
+
+脱水后的贴子
+![thread](dewater_thread.png)
---
+++ discuz_dewater.js
@@ -0,0 +1,59 @@
+function banner_path() {
+    return 'div#pt';
+}
+
+function extract_floor_info(bot) {
+    var info = bot.parent();
+    var re = new Object;
+    re["poster"] = info.find('div.authi a.xw1').text();
+    re["time"] = info.find('div.authi em').text().replace('发表于','');
+    re["id"] = info.find('div.pi em').text().replace(/\D.*$/,'');
+    re["content"] = info.find('td.t_f').html().
+        replace(/<[^>]+style="display:none"[^>]*>[^<]+<[^>]+>/g, '').
+        replace(/<[^>]+class="jammer"[^>]*>[^<]+<[^>]+>/g, '').
+        replace(/<\/?font[^>]*>/g, '');
+    re["word_num"] = re["content"].replace('<[^>]+>','').length;
+    return re;
+}
+
+function floor_path() {
+    //return 'table.plhin';
+    return 'div#postlist>div>table';
+}
+
+function page_charset() {
+    var h = $('head').html();
+    var h_m = h.match(/.*charset=(.*?)".*/);
+    if(!h_m) return 'gb2312';
+    return h_m[1];
+}
+
+function get_topic_name() {
+    return $('#thread_subject').text();
+}
+
+function get_page_num() {
+    var pg = $('div#pgt');
+    if(!pg) return;
+    var num = pg.html().replace(/<[^>]*>/g,' ');
+    var num_m = num.match(/[\D\s]*(\d+)\s*(页|下一页)/);
+    if(!num_m) return;
+    return num_m[1];
+}
+
+function format_thread_url_1st(url) {
+    url = url.replace(/\/thread-(\d+)-\d+-1.html$/, '/forum.php?mod=viewthread&tid=$1&page=1');
+    url = url.replace(/#.*$/, '').replace(/&extra=[^&]*/,'').replace(/&page=\d+/, '&page=1');
+
+    if(! url.match(/&page=\d+/)){
+        url = url.concat('&page=1');
+    }
+
+    return url;
+}
+
+function format_thread_url_ith(url,i)  {
+    var j = i.toString();
+    var n_url = url.replace(/&page=\d+/, '&page='+ j);
+    return n_url;
+}
